d935362d09f6317805dd9eb7effc434ba31eed8f
Sandip <sandip.pokar2724@gmail.com>
2022-07-20 19:49:04 +0530

    codeswear first commit checkout done

---
+++ pages/api/pincode.js
@@ -0,0 +1,6 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+    res.status(200).json([123456, 360004, 234440, 722211, 360005]);
+  }
+  
